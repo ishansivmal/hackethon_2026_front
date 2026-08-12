4b17fba46904f44378f9823760ac232d492e984d
Krishani-imalka <krishani08imalka@gmail.com>
2026-08-13 05:29:07 +0530

    create the user management page crud

--- src/api/admin.js
+++ src/api/admin.js
@@ -2,7 +2,12 @@ import api from './axios'
 
 export const getUsers = () => api.get('/admin/users')
 
+export const createUser = (userData) => api.post('/admin/users', userData)
+
 export const updateUserRole = (id, role) =>
   api.put(`/admin/users/${id}/role`, { role })
+
+export const updateUser = (id, userData) =>
+  api.put(`/admin/users/${id}`, userData)
 
 export const deleteUser = (id) => api.delete(`/admin/users/${id}`)
